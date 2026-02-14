5656b2feed6b036c0543b075d79fe6732bb8e0fa
Cambrian Minds <justin@cambrianminds.com>
2026-02-13 21:04:26 -0500

    pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/maid-website",
+  assetPrefix: "/maid-website/",
   /* config options here */
 };
 
